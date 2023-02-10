e819240c4dc1e08e505bc0c2fb3b76350fd40d9f
rmsdud0527 <rmsdud0527@naver.com>
2023-02-10 20:00:04 +0900

    첫 페이지까지 완료, 도서명만

--- interpark_practice.py
+++ interpark_practice.py
@@ -11,35 +11,15 @@ import requests
 
 driver = webdriver.Chrome("C:/Users/김근영/chromedriver_win32.zip/chromedriver.exe")
 
-#driver = webdriver.Chrome(ChromeDriverManager().install())
-
-
 # 인터파크 베스트셀러 웹페이지를 가져옵니다.
+driver.get("http://book.interpark.com/display/collectlist.do?_method=BestsellerHourNew201605&bestTp=1&dispNo=028#")
+time.sleep(1)
 
-url = "http://book.interpark.com/display/collectlist.do?_method=BestsellerHourNew201605&bestTp=1&dispNo=028#"
-response = driver.get(url)
+driver.find_element(By.CSS_SELECTOR, "#cateTabId2 > a > span").click() #일단은 주간으로 함.
+time.sleep(1)
+soup = BeautifulSoup(driver.page_source, 'html.parser')
+books = soup.select('div.itemName')
 
-# driver.get("http://book.interpark.com/display/collectlist.do?_method=BestsellerHourNew201605&bestTp=1&dispNo=028#")
-# time.sleep(2)
-
-
-# driver.find_element(By.CSS_SELECTOR, "#cateTabId2 > a > span").click() #일단은 주간으로 함.
-# time.sleep(2)
-# bsObject = BeautifulSoup(driver.page_source, 'html.parser')
-
-
-response = requests.get(url)
-dom = BeautifulSoup(response.text, "html.parser")
-type(dom)
-
-
-elements = dom.select("#content > div.rankBestWrapper > div.rankBestContainer > div.rankBestContents > div > div.rankBestContentList > ol > li:nth-child(1)")
-items = []
-for element in elements:
-    data = {
-        "title" : element.select_one("div.itemName > strong").text
-    }
-
-best_interpark = pd.DataFrame(items)
-best_interpark.to_csv('./best_interpark.csv', index = False)
-print(best_interpark)
+for book in books:
+    rank = book.select('strong')[0].text
+    print(rank)
